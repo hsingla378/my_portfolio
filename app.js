424f837a1d212994048d35583231500b657dde6f
hsingla378 <hsingla378@gmail.com>
2022-12-02 13:09:19 +0530

    made resposive skills section and hero section

---
+++ app.js
@@ -0,0 +1,16 @@
+const exp = require("constants")
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const path = require("path")
+
+const port = process.env.PORT || 3000
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './view', 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Server is started on port ${port}`)
+})
