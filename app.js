88cdbd243043c46315f576904341b13e8f9d9600
hsingla378 <hsingla378@gmail.com>
2022-12-03 17:37:40 +0530

    made public folder to link css and images

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const path = require("path")
 const port = process.env.PORT || 3000
 app.use(express.static("public"))
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, './view', 'index.html'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
 })
 
 app.listen(port, () => {
