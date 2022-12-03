4ea9f9246c13f8eac740c644bd160f4c86b0080e
hsingla378 <hsingla378@gmail.com>
2022-12-03 18:29:12 +0530

    changed app.use
    add the express.static with dirname and path.join to show css and images

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express()
 require("dotenv").config()
 const path = require("path")
 
+app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 
 const port = process.env.PORT || 3000
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
